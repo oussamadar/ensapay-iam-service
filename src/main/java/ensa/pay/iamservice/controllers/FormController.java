a4af9464c5c7921ad26bddab0cbbb333fa57023b
dariaoui <oussama.darioui@gmail.com>
2021-01-31 16:14:34 +0100

    update formController

--- src/main/java/ensa/pay/iamservice/controllers/FormController.java
+++ src/main/java/ensa/pay/iamservice/controllers/FormController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("iam/gitform")
+@RequestMapping("iam/form")
 public class FormController {
 
     @Autowired
